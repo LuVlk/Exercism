974d00e0812416ff5622d0445ba6e6f01bb79504
Lukas Volk <lukas_volk@yahoo.de>
2022-05-01 17:28:47 +0200

    [lens-person] complete exercise

--- scala/lens-person/src/main/scala/LensPerson.scala
+++ scala/lens-person/src/main/scala/LensPerson.scala
@@ -9,49 +9,45 @@ case class Lens[A /* object to act on */, B /* value to act for */](get: A => B,
 
 object LensPerson {
   case class Person(_name: Name, _born: Born, _address: Address)
-  val personAdressLens = Lens[Person, Address](
+  val personAddressLens: Lens[Person, Address] = Lens(
     get = p => p._address,
     set = (a, p) => p.copy(_address = a)
   )
-  val personBornLens = Lens[Person, Born](
+  val personBornLens: Lens[Person, Born] = Lens(
     get = p => p._born,
     set = (b, p) => p.copy(_born = b)
   )
 
   case class Name(_foreNames: String /*Space separated*/ , _surName: String)
 
+  case class Address(_street: String, _houseNumber: Int, _place: String /*Village / city*/ , _country: String)
+  val addressStreetLens: Lens[Address, String] = Lens(a => a._street, (s, a) => a.copy(_street = s))
+
   // Value of java.time.LocalDate.toEpochDay
   type EpochDay = Long
 
   case class Born(_bornAt: Address, _bornOn: EpochDay)
-  val bornAddressLens = Lens[Born, Address](
+  val bornAddressLens: Lens[Born, Address] = Lens(
     get = b => b._bornAt,
     set = (a, b) => b.copy(_bornAt = a)
   )
-  val bornStreetLens = bornAddressLens compose addressStreetLens
+  val bornStreetLens: Lens[Born, String] = bornAddressLens compose addressStreetLens
 
-  val bornOnLens = Lens[Born, EpochDay](
+  val bornOnLens: Lens[Born, EpochDay] = Lens(
     get = b => b._bornOn,
     set = (bo, b) => b.copy(_bornOn = bo)
   )
-  val epochDayMonthLens = Lens[EpochDay, Int](
+  val epochDayMonthLens: Lens[EpochDay, Int] = Lens(
     get = ed => ofEpochDay(ed).getMonthValue,
     set = (m, ed) => ofEpochDay(ed).withMonth(m).toEpochDay
   )
-
-  case class Address(_street: String, _houseNumber: Int, _place: String /*Village / city*/ , _country: String)
-  val addressStreetLens = Lens[Address, String](a => a._street, (s, a) => a.copy(_street = s))
-
-  // Valid values of Gregorian are those for which 'java.time.LocalDate.of'
-  // returns a valid LocalDate.
-  case class Gregorian(_year: Int, _month: Int, _dayOfMonth: Int)
 
   // Implement these.
 
   val bornStreet: Born => String = bornStreetLens.get
 
   val setCurrentStreet: String => Person => Person =
-    street => (p: Person) => (personAdressLens compose addressStreetLens).set(street, p)
+    street => (p: Person) => (personAddressLens compose addressStreetLens).set(street, p)
 
   val setBirthMonth: Int => Person => Person =
     month => (p: Person) => (personBornLens compose bornOnLens compose epochDayMonthLens).set(month, p)
@@ -59,7 +55,7 @@ object LensPerson {
   // Transform both birth and current street names.
   val renameStreets: (String => String) => Person => Person =
     renameFunc => (p: Person) => {
-      val personAdressStreetLens = personAdressLens compose addressStreetLens
+      val personAdressStreetLens = personAddressLens compose addressStreetLens
       val personBornStreetLens = personBornLens compose bornStreetLens
 
       val pAdressStreetRenamed = personAdressStreetLens.set(renameFunc(personAdressStreetLens.get(p)), p)
