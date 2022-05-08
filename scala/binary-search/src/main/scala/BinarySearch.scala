3bbc55fad9a2bf1b84d3de598ca7b273655fe11b
Lukas Volk <lukas_volk@yahoo.de>
2022-05-08 18:19:12 +0200

    [binary-search] using index based recursive approach

--- scala/binary-search/src/main/scala/BinarySearch.scala
+++ scala/binary-search/src/main/scala/BinarySearch.scala
@@ -1,12 +1,15 @@
 import scala.annotation.tailrec
 
 object BinarySearch {
-  @tailrec
-  def find[A](list: List[A], element: A, offset: Int = 0)(implicit ordering: Ordering[A]): Option[Int] = {
-    if (list.isEmpty) return None
-    val mid = list.length / 2
-    if (list(mid) == element) return Some(offset + mid)
-    if (ordering.lt(element, list(mid))) find(list.slice(0, mid), element, offset)
-    else find(list.slice(mid + 1, list.length + 1), element, offset + mid + 1)
+  def find[A](list: List[A], element: A)(implicit ordering: Ordering[A]): Option[Int] = {
+    @tailrec
+    def go(low: Int, high: Int): Option[Int] = {
+      if (low > high) return None
+      val mid = (low + high) / 2
+      if (list(mid) == element) return Some(mid)
+      if (ordering.lt(element, list(mid))) go(low, mid - 1)
+      else go(mid + 1, high)
+    }
+    go(0, list.length - 1)
   }
 }
