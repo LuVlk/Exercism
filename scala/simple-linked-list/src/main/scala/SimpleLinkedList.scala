9811285ed70e135dbc99154f243dda0a6b4d8f6d
Lukas Volk <lukas_volk@yahoo.de>
2022-05-31 11:04:50 +0200

    [scala/simple-linked-list] initial solution

--- scala/simple-linked-list/src/main/scala/SimpleLinkedList.scala
+++ scala/simple-linked-list/src/main/scala/SimpleLinkedList.scala
@@ -6,3 +6,39 @@ trait SimpleLinkedList[T] {
   def reverse: SimpleLinkedList[T]
   def toSeq: Seq[T]
 }
+
+object SimpleLinkedList {
+
+  private case class Node[T](var _value: Option[T] = None) extends SimpleLinkedList[T] {
+    var _next: Option[SimpleLinkedList[T]] = None
+
+    override def isEmpty: Boolean = _value.isEmpty
+
+    override def value: T = _value.get
+
+    override def add(item: T): SimpleLinkedList[T] = {
+      if (isEmpty) _value = Some(item)
+      else if (next.isEmpty) {
+        val newList = apply(item)
+        _next = Some(newList)
+      }
+      else next.add(item)
+      this
+    }
+
+    override def next: SimpleLinkedList[T] = _next.getOrElse(Node())
+
+    // creates in memory copy. Improve !
+    override def reverse: SimpleLinkedList[T] = fromSeq(toSeq.reverse)
+
+    override def toSeq: Seq[T] = {
+      if (isEmpty) Seq()
+      else Seq(_value.get) ++ next.toSeq
+    }
+  }
+
+  def apply[T](items: T*): SimpleLinkedList[T] =
+    items.foldLeft[SimpleLinkedList[T]](Node())((list, item) => list.add(item))
+
+  def fromSeq[T](items: Seq[T]): SimpleLinkedList[T] = apply(items: _*)
+}
