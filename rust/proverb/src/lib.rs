04d5b07d7d8a69e00d3b922466f12ffc5eed452b
Lukas Volk <lukas_volk@yahoo.de>
2024-04-01 13:01:08 +0200

    [rust/proverb] removing generic pairwise iteration function

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,21 +1,11 @@
-use std::iter;
-
-fn pairwise<I>(right: I) -> impl Iterator<Item = (Option<I::Item>, I::Item)>
-where
-    I: IntoIterator + Clone,
-{
-    let left = iter::once(None).chain(right.clone().into_iter().map(Some));
-    left.zip(right)
-}
-
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut pairs = pairwise(list);
-    let head = pairs.next().map(|(_, head)| head);
+    let want = list.into_iter();
+    let mut lost = list.into_iter();
+    let head = lost.next();
 
-    pairs
-        .flat_map(|(want, lost)| want.map(|w| (w, lost)))
-        .fold(String::new(), |acc, (want, lost)| {
-            acc + &format!("For want of a {} the {} was lost.\n", want, lost)
-        }) 
-        + &head.map(|h| format!("And all for the want of a {}.", h)).unwrap_or("".to_owned())
+    want.zip(lost).fold(String::new(), |acc, (want, lost)| {
+        acc + &format!("For want of a {} the {} was lost.\n", want, lost)
+    }) + &head
+        .map(|h| format!("And all for the want of a {}.", h))
+        .unwrap_or("".to_owned())
 }
