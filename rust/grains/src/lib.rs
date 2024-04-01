0220ddf2e685de693e8c029d38d59765ac9056ba
Lukas Volk <lukas_volk@yahoo.de>
2024-04-01 12:08:23 +0200

    [rust/grains] optimizing and benchmarking

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,8 +1,33 @@
+#![feature(test)]
+extern crate test;
+
 pub fn square(s: u32) -> u64 {
     assert!((1..=64).contains(&s), "Square must be between 1 and 64");
-    (2 as u64).pow(s - 1)
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    (1..=64).map(square).sum()
+    u64::MAX
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn one_bench(b: &mut Bencher) {
+        b.iter(|| square(1));
+    }
+
+    #[bench]
+    fn sixty_four_bench(b: &mut Bencher) {
+        b.iter(|| square(64));
+    }
+
+    #[bench]
+    fn total_bench(b: &mut Bencher) {
+        b.iter(|| total());
+    }
+
 }
