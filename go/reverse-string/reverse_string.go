986a8f6867342d73da44baa8f95aa1f39a89bd80
Lukas Volk <lukas_volk@yahoo.de>
2024-05-01 23:42:33 +0200

    [go/reverse-string] correct but shitty impl

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,9 +1,9 @@
 package reverse
 
 func Reverse(input string) string {
-	runes := []rune(input)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	out := ""
+	for _, c := range input {
+		out = string(c) + out
 	}
-	return string(runes)
+	return out
 }
