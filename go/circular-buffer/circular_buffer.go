db872055af0d836b26e47426ca3e71de8e945ed3
Lukas Volk <lukas_volk@yahoo.de>
2024-10-13 13:55:14 +0200

    [go/circular-buffer] optimize performance

--- go/circular-buffer/circular_buffer.go
+++ go/circular-buffer/circular_buffer.go
@@ -6,29 +6,24 @@ import (
 
 type Buffer struct {
 	data []byte
+	size int
 }
 
 func NewBuffer(size int) *Buffer {
-	return &Buffer{data: make([]byte, 0, size)}
-}
-
-func (b *Buffer) pop() byte {
-	c := b.data[0]
-	tail := make([]byte, len(b.data)-1, cap(b.data))
-	copy(tail, b.data[1:])
-	b.data = tail
-	return c
+	return &Buffer{data: make([]byte, 0, size), size: size}
 }
 
 func (b *Buffer) ReadByte() (byte, error) {
 	if len(b.data) == 0 {
 		return 0, errors.New("Buffer is empty")
 	}
-	return b.pop(), nil
+	c := b.data[0]
+	b.data = b.data[1:]
+	return c, nil
 }
 
 func (b *Buffer) WriteByte(c byte) error {
-	if len(b.data) == cap(b.data) {
+	if len(b.data) == b.size {
 		return errors.New("Buffer is full")
 	}
 	b.data = append(b.data, c)
@@ -36,8 +31,8 @@ func (b *Buffer) WriteByte(c byte) error {
 }
 
 func (b *Buffer) Overwrite(c byte) {
-	if len(b.data) == cap(b.data) {
-		b.pop()
+	if len(b.data) == b.size {
+		b.data = b.data[1:]
 	}
 	b.WriteByte(c)
 }
@@ -56,15 +51,15 @@ cpu: 13th Gen Intel(R) Core(TM) i7-13700H
 === RUN   BenchmarkOverwrite
 BenchmarkOverwrite
 BenchmarkOverwrite-20
-17386164                71.41 ns/op     243472592.85 MB/s            111 B/op          0 allocs/op
+349730750                4.152 ns/op    84228690196.28 MB/s            1 B/op          0 allocs/op
 PASS
-ok      standard        1.379s
+ok      standard        1.881s
 
 === RUN   BenchmarkWriteRead
 BenchmarkWriteRead
 BenchmarkWriteRead-20
-21257222                76.97 ns/op     276168292.25 MB/s            112 B/op          1 allocs/op
+215245246                5.795 ns/op    37143302142.77 MB/s            1 B/op          0 allocs/op
 PASS
-ok      standard        1.744s
+ok      standard        1.902s
 
 */
