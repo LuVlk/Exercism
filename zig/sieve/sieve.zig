bfef2036c3ff7edc61e4e03bf776049d890c41fe
Lukas Volk <lukas_volk@yahoo.de>
2024-10-20 17:13:43 +0200

    [zig/sieve] using bitset

--- zig/sieve/sieve.zig
+++ zig/sieve/sieve.zig
@@ -1,23 +1,18 @@
 const std = @import("std");
 
 pub fn primes(buffer: []u32, comptime limit: u32) []u32 {
-    var sieve = [_]bool{true} ** (limit + 1);
+    var sieve = std.StaticBitSet(limit + 1).initFull();
     var prime_cntr: u32 = 0;
-
     var i: u32 = 2;
-    while (i <= limit) {
-        if (sieve[i]) {
-            var j = i * i;
-            while (j <= limit) {
-                sieve[j] = false;
-                j += i;
-            }
 
-            buffer[prime_cntr] = i;
-            prime_cntr += 1;
-        }
+    while (i <= limit) : (i += 1) {
+        if (!sieve.isSet(i)) continue;
 
-        i += 1;
+        var j = i * i;
+        while (j <= limit) : (j += i) sieve.unset(j);
+
+        buffer[prime_cntr] = i;
+        prime_cntr += 1;
     }
 
     return buffer[0..prime_cntr];
