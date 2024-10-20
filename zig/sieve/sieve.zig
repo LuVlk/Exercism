12f7062157292caf868329ba5fd86fc175d2bd86
Lukas Volk <lukas_volk@yahoo.de>
2024-10-20 17:26:54 +0200

    [zig/sieve] using heap allocation

--- zig/sieve/sieve.zig
+++ zig/sieve/sieve.zig
@@ -1,15 +1,21 @@
 const std = @import("std");
+const heap = std.heap.page_allocator;
 
-pub fn primes(buffer: []u32, comptime limit: u32) []u32 {
-    var sieve = std.StaticBitSet(limit + 1).initFull();
+pub fn primes(buffer: []u32, limit: u32) []u32 {
+    var sieve = heap.alloc(bool, limit + 1) catch {
+        @panic("allocation failed");
+    };
+    defer heap.free(sieve);
+    @memset(sieve, true);
+
     var prime_cntr: u32 = 0;
     var i: u32 = 2;
 
     while (i <= limit) : (i += 1) {
-        if (!sieve.isSet(i)) continue;
+        if (!sieve[i]) continue;
 
         var j = i * i;
-        while (j <= limit) : (j += i) sieve.unset(j);
+        while (j <= limit) : (j += i) sieve[j] = false;
 
         buffer[prime_cntr] = i;
         prime_cntr += 1;
