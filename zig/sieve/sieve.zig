a5c85ac4a22d73c45cdfb27e72b92d5269350c44
Lukas Volk <lukas_volk@yahoo.de>
2024-10-20 16:54:01 +0200

    [zig/sieve] initial solution

---
+++ zig/sieve/sieve.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+pub fn primes(buffer: []u32, limit: u32) []u32 {
+    var sieve = [_]bool{true} ** 1001;
+    var prime_cntr: u32 = 0;
+
+    var i: u32 = 2;
+    while (i <= limit) {
+        if (sieve[i]) {
+            var j = i * i;
+            while (j <= limit) {
+                sieve[j] = false;
+                j += i;
+            }
+
+            buffer[prime_cntr] = i;
+            prime_cntr += 1;
+        }
+
+        i += 1;
+    }
+
+    return buffer[0..prime_cntr];
+}
