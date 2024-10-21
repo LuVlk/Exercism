5591472313c8baea862dfc5c02670bcc47e838c6
Lukas Volk <lukas_volk@yahoo.de>
2024-10-21 22:18:36 +0200

    [zig/pangram] reduce nesting

--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -4,11 +4,10 @@ const palloc = std.heap.page_allocator;
 pub fn isPangram(str: []const u8) bool {
     var map = std.AutoHashMap(u8, bool).init(palloc);
     for (str) |c| {
-        if (std.ascii.isAlphabetic(c)) {
-            map.put(std.ascii.toLower(c), false) catch {
-                @panic("allocation failed");
-            };
-        }
+        if (!std.ascii.isAlphabetic(c)) continue;
+        map.put(std.ascii.toLower(c), false) catch {
+            @panic("allocation failed");
+        };
     }
     return map.count() == 26;
 }
