07b19faaa571f2c15fded58199a6ba8179dd3d89
Lukas Volk <lukas_volk@yahoo.de>
2024-10-21 22:25:17 +0200

    [zig/pangram] using BitSet

--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -2,12 +2,12 @@ const std = @import("std");
 const palloc = std.heap.page_allocator;
 
 pub fn isPangram(str: []const u8) bool {
-    var map = std.AutoHashMap(u8, bool).init(palloc);
+    var set = std.bit_set.IntegerBitSet(26).initEmpty();
+
     for (str) |c| {
         if (!std.ascii.isAlphabetic(c)) continue;
-        map.put(std.ascii.toLower(c), false) catch {
-            @panic("allocation failed");
-        };
+        set.set(std.ascii.toLower(c) - 'a');
     }
-    return map.count() == 26;
+
+    return set.count() == 26;
 }
