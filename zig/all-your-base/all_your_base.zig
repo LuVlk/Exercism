2d53eea870d8d011383dcd5cf150b8ba54fe3ab8
Lukas Volk <lukas_volk@yahoo.de>
2024-10-27 22:46:50 +0100

    [zig/all-your-base] use ArrayList

--- zig/all-your-base/all_your_base.zig
+++ zig/all-your-base/all_your_base.zig
@@ -20,53 +20,36 @@ pub fn convert(
 }
 
 fn fromDigits(digits: []const u32, input_base: u32) (ConversionError)!u32 {
-    if (input_base < 2) {
-        return ConversionError.InvalidInputBase;
-    }
+    if (input_base < 2) return ConversionError.InvalidInputBase;
 
     var n: u32 = 0;
     for (digits) |d| {
-        if (!(d >= 0 and d < input_base)) {
-            return ConversionError.InvalidDigit;
-        }
+        if (!(d >= 0 and d < input_base)) return ConversionError.InvalidDigit;
         n = n * input_base + d;
     }
     return n;
 }
 
 fn toDigits(allocator: mem.Allocator, number: u32, output_base: u32) (mem.Allocator.Error || ConversionError)![]u32 {
-    if (output_base < 2) {
-        return ConversionError.InvalidOutputBase;
-    }
+    if (output_base < 2) return ConversionError.InvalidOutputBase;
 
     var exp: u32 = 0;
     while (std.math.pow(u32, output_base, exp) <= number) {
         exp += 1;
     }
 
-    var digits = try allocator.alloc(u32, exp);
-    if (exp == 0) {
-        digits = try allocator.alloc(u32, 1);
-        digits[0] = 0;
-        return digits;
-    }
+    var digits = try std.ArrayList(u32).initCapacity(allocator, exp);
+    defer digits.deinit();
 
     var n = number;
-    for (0..digits.len) |i| {
-        digits[i] = n % output_base;
+    for (0..digits.capacity) |_| {
+        digits.insertAssumeCapacity(0, n % output_base);
         n /= output_base;
     }
 
-    var i: usize = 0;
-    var j: usize = digits.len - 1;
-    while (i < j) : ({
-        i += 1;
-        j -= 1;
-    }) {
-        const iv = digits[i];
-        digits[i] = digits[j];
-        digits[j] = iv;
+    if (exp == 0) {
+        try digits.append(0);
     }
 
-    return digits;
+    return try digits.toOwnedSlice();
 }
