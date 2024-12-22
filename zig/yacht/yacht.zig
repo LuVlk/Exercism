6b111bb7f615895db53213aaab30b32954b62a5c
Lukas Volk <lukas_volk@yahoo.de>
2024-12-22 11:39:31 +0100

    [zig/yacht] optimized for short and concise solution

--- zig/yacht/yacht.zig
+++ zig/yacht/yacht.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
-pub const Category = enum {
-    ones,
-    twos,
-    threes,
-    fours,
-    fives,
-    sixes,
+pub const Category = enum(u4) {
+    ones = 1,
+    twos = 2,
+    threes = 3,
+    fours = 4,
+    fives = 5,
+    sixes = 6,
     full_house,
     four_of_a_kind,
     little_straight,
@@ -15,122 +15,28 @@ pub const Category = enum {
     yacht,
 };
 
+fn sum(dice: [5]u3) u32 {
+    return @as(u32, dice[0]) + dice[1] + dice[2] + dice[3] + dice[4];
+}
+
 pub fn score(dice: [5]u3, category: Category) u32 {
+    var sorted: [5]u3 = undefined;
+    @memcpy(&sorted, &dice);
+    std.mem.sort(u3, &sorted, {}, std.sort.asc(u3));
+
     return switch (category) {
-        .ones => score_num(dice, 1),
-        .twos => score_num(dice, 2),
-        .threes => score_num(dice, 3),
-        .fours => score_num(dice, 4),
-        .fives => score_num(dice, 5),
-        .sixes => score_num(dice, 6),
-        .full_house => full_house(dice),
-        .four_of_a_kind => four_of_a_kind(dice),
-        .little_straight => little_straight(dice),
-        .big_straight => big_straight(dice),
-        .choice => choice(dice),
-        .yacht => yacht(dice),
+        .ones, .twos, .threes, .fours, .fives, .sixes => @truncate(@intFromEnum(category) * std.mem.count(u3, &sorted, &[_]u3{@truncate(@intFromEnum(category))})),
+
+        .little_straight => if (std.mem.eql(u3, &[_]u3{ 1, 2, 3, 4, 5 }, &sorted)) 30 else 0,
+
+        .big_straight => if (std.mem.eql(u3, &[_]u3{ 2, 3, 4, 5, 6 }, &sorted)) 30 else 0,
+
+        .choice => sum(dice),
+
+        .four_of_a_kind => if (sorted[1] == sorted[4] or sorted[0] == sorted[3]) @as(u32, 4) * sorted[1] else 0,
+
+        .yacht => if (sorted[0] == sorted[4]) return 50 else 0,
+
+        .full_house => if (sorted[0] == sorted[1] and ((sorted[1] == sorted[2]) != (sorted[2] == sorted[3])) and sorted[3] == sorted[4]) sum(dice) else 0,
     };
-}
-
-fn score_num(dice: [5]u3, num: u3) u32 {
-    var out: u32 = 0;
-
-    for (dice) |d| {
-        if (d == num) out += d;
-    }
-
-    return out;
-}
-
-fn full_house(dice: [5]u3) u32 {
-    var out: u32 = 0;
-
-    var g1: u3 = 0;
-    var g1Cnt: u3 = 0;
-    var g2: u3 = 0;
-    var g2Cnt: u3 = 0;
-
-    for (dice) |d| {
-        if (g1 == 0 or g1 == d) {
-            g1 = d;
-            g1Cnt += 1;
-            out += d;
-        }
-        else if (g2 == 0 or g2 == d) {
-            g2 = d;
-            g2Cnt += 1;
-            out += d;
-        }
-    }
-
-    const cnts = .{g1Cnt, g2Cnt};
-    if (std.meta.eql(cnts, .{2, 3}) or std.meta.eql(cnts, .{3, 2})) {
-        return out;
-    }
-
-    return 0;
-}
-
-fn four_of_a_kind(dice: [5]u3) u32 {
-    var out: u32 = 0;
-
-    var sortable: [5]u3 = undefined;
-    @memcpy(&sortable, &dice);
-    std.mem.sort(u3, &sortable, {}, comptime std.sort.asc(u3));
-
-    const relevant: []u3 = switch (sortable[0] == sortable[1]) {
-        true => sortable[0..4],
-        false => sortable[1..5]
-    };
-
-    const key: u3 = relevant[0];
-    for (relevant) |d| {
-        if (d != key) return 0;
-        out += d;
-    }
-
-    return out;
-}
-
-
-fn little_straight(dice: [5]u3) u32 {
-
-    var sorted: [5]u3 = undefined;
-    @memcpy(&sorted, &dice);
-    std.mem.sort(u3, &sorted, {}, comptime std.sort.asc(u3));
-
-    if (std.meta.eql(sorted, .{1, 2, 3, 4, 5})) {
-        return 30;
-    }
-    return 0;
-}
-
-fn big_straight(dice: [5]u3) u32 {
-
-    var sorted: [5]u3 = undefined;
-    @memcpy(&sorted, &dice);
-    std.mem.sort(u3, &sorted, {}, comptime std.sort.asc(u3));
-
-    if (std.meta.eql(sorted, .{2, 3, 4, 5, 6})) {
-        return 30;
-    }
-    return 0;
-}
-
-
-fn choice(dice: [5]u3) u32 {
-    var out: u32 = 0;
-    for (dice) |d| {
-        out += d;
-    }
-    return out;
-}
-
-
-fn yacht(dice: [5]u3) u32 {
-    const key: u3 = dice[0];
-    for (dice) |d| {
-        if (d != key) return 0;
-    }
-    return 50;
 }
