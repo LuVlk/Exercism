0ba5d17cedafa7ffa78dde86d315bcd0f95260f1
Lukas Volk <lukas_volk@yahoo.de>
2025-01-04 21:20:34 +0100

    [zig/matching-brackets] proper solution

--- zig/matching-brackets/matching_brackets.zig
+++ zig/matching-brackets/matching_brackets.zig
@@ -2,51 +2,18 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn isBalanced(allocator: mem.Allocator, s: []const u8) !bool {
-    _ = allocator;
-    if (s.len == 0) return true;
-    return findMatchingBracketsWithRespectToInnerPairs(s, null) != null;
-}
+    var stack = std.ArrayList(u8).init(allocator);
+    defer stack.deinit();
 
-fn findMatchingBracketsWithRespectToInnerPairs(s: []const u8, match: ?u8) ?usize {
-
-    if (s.len == 0) return null;
-
-    var i: usize = 0;
-    while (i <= s.len-1) : (i += 1) {
-        if (match) |c| {
-            if (s[i] == c) {
-                return i;
-            }
-        }
-
-        if (s[i] == '{') {
-            if (findMatchingBracketsWithRespectToInnerPairs(s[i+1..], '}')) |found| {
-                i += found+1;
-            } else {
-                return null;
-            }
-        }
-        else if (s[i] == '[') {
-            if (findMatchingBracketsWithRespectToInnerPairs(s[i+1..], ']')) |found| {
-                i += found+1;
-            } else {
-                return null;
-            }
-        }
-        else if (s[i] == '(') {
-            if (findMatchingBracketsWithRespectToInnerPairs(s[i+1..], ')')) |found| {
-                i += found+1;
-            } else {
-                return null;
-            }
-        }
-        else if (s[i] == '}' or s[i] == ']' or s[i] == ')') {
-            return null;
+    for (s) |c| {
+        switch (c) {
+            '[' => try stack.append(']'),
+            '{' => try stack.append('}'),
+            '(' => try stack.append(')'),
+            ']', '}', ')' => if (stack.popOrNull() != c) return false,
+            else => {},
         }
     }
 
-    if (match == null) {
-        return 0;
-    }
-    return null;
+    return stack.items.len == 0;
 }
